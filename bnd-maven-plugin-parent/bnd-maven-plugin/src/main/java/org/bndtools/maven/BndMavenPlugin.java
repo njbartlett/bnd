96a9754ed54b1579f3c4959eb1f44687fafc79fa
Neil Bartlett <njbartlett@gmail.com>
2015-03-09 22:11:34 +0000

    Import bnd-maven-plugin contribution from Salesforce.com
    This code donated under the terms of Apache License v2 as confirmed by
    email from Stefan Derdak <sderdak@salesforce.com> on March 5 2015.
    
    Signed-off-by: Neil Bartlett <neil.bartlett@paremus.com>

--- bnd-maven-plugin-parent/bnd-maven-plugin/src/main/java/org/bndtools/maven/BndMavenPlugin.java
+++ bnd-maven-plugin-parent/bnd-maven-plugin/src/main/java/org/bndtools/maven/BndMavenPlugin.java
@@ -16,12 +16,16 @@ package org.bndtools.maven;
  * limitations under the License.
  */
 
+import static aQute.lib.io.IO.getFile;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
@@ -36,7 +40,9 @@ import org.apache.maven.project.MavenProject;
 import aQute.bnd.build.Project;
 import aQute.bnd.osgi.Builder;
 import aQute.bnd.osgi.Constants;
+import aQute.bnd.osgi.FileResource;
 import aQute.bnd.osgi.Jar;
+import aQute.bnd.osgi.Resource;
 import aQute.lib.io.IO;
 
 @Mojo(name = "bnd-process", defaultPhase = LifecyclePhase.PROCESS_CLASSES, requiresDependencyResolution = ResolutionScope.COMPILE)
@@ -54,9 +60,6 @@ public class BndMavenPlugin extends AbstractMojo {
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
 	private MavenProject project;
 	
-	@Parameter(defaultValue = "${project.build.directory}/bnd-tmp", readonly = true)
-	private File tempDir;
-	
 	@Parameter(property = "bnd.trace", defaultValue = "false", readonly = true)
 	private boolean trace;
 
@@ -64,7 +67,7 @@ public class BndMavenPlugin extends AbstractMojo {
 		File bndFile = new File(project.getBasedir(), Project.BNDFILE);
 		
 		Builder builder = new Builder();
-		builder.setTrace(true);
+		builder.setTrace(trace);
 		try {
 			if (bndFile.isFile())
 				builder.setProperties(bndFile);
@@ -102,15 +105,22 @@ public class BndMavenPlugin extends AbstractMojo {
 			Jar bndJar = builder.build();
 			
 			// Generate errors and warnings
-			List<String> errors = builder.getErrors();
-			if (errors != null) for (String error : errors) {
-				getLog().error(error);
-			}
 			List<String> warnings = builder.getWarnings();
 			if (warnings != null) for (String warning : warnings) {
 				getLog().warn(warning);
 			}
-			
+			List<String> errors = builder.getErrors();
+			if (errors != null && !errors.isEmpty()) {
+				for (String error : errors) {
+					getLog().error(error);
+				}
+
+				if (errors.size() == 1)
+					throw new MojoExecutionException(errors.get(0));
+				else
+					throw new MojoExecutionException("Errors in bnd processing, see log for details.");
+			}
+
 			// Output manifest to <classes>/META-INF/MANIFEST.MF
 			Files.createDirectories(manifestPath.toPath().getParent());
 			FileOutputStream manifestOut = new FileOutputStream(manifestPath);
@@ -120,17 +130,42 @@ public class BndMavenPlugin extends AbstractMojo {
 				manifestOut.close();
 			}
 
-			// Expand Jar into target/bnd-temp
-			Files.createDirectories(tempDir.toPath());
-			bndJar.expand(tempDir);
-			
-			// Copy from target/bnd-temp back to target/classes
-			IO.copy(tempDir, classesDir);
+			// Expand Jar into target/classes
+			expandJar(bndJar, classesDir);
 
 		} catch (Exception e) {
 			throw new MojoExecutionException("bnd error", e);
 		} finally {
 			builder.close();
+		}
+	}
+	
+	private static void expandJar(Jar jar, File dir) throws Exception {
+		dir = dir.getAbsoluteFile();
+		if (!dir.exists() && !dir.mkdirs()) {
+			throw new IOException("Could not create directory " + dir);
+		}
+		if (!dir.isDirectory()) {
+			throw new IllegalArgumentException("Not a dir: " + dir.getAbsolutePath());
+		}
+
+		for (Map.Entry<String,Resource> entry : jar.getResources().entrySet()) {
+			File outFile = getFile(dir, entry.getKey());
+			File outDir = outFile.getParentFile();
+			if (!outDir.exists() && !outDir.mkdirs()) {
+				throw new IOException("Could not create directory " + outDir);
+			}
+
+			// Skip the copy if the source and target file are the same
+			Resource resource = entry.getValue();
+			if (resource instanceof FileResource) {
+				@SuppressWarnings("resource")
+				FileResource fr = (FileResource) resource;
+				if (outFile.equals(fr.getFile()))
+					continue;
+			}
+
+			IO.copy(entry.getValue().openInputStream(), outFile);
 		}
 	}
 
